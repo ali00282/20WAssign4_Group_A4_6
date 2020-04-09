d6659ec3072025920e34940d89b6b8ac1ff11881
Sirisha Jayampu <jaya0029@algonquinlive.com>
2020-04-09 14:52:33 -0400

    Fix user1 password and security role mapping for user1

--- 20WAssign4_Group_A4_6/src/main/resources/META-INF/Scripts/Script-1.sql
+++ 20WAssign4_Group_A4_6/src/main/resources/META-INF/Scripts/Script-1.sql
@@ -5,8 +5,9 @@ INSERT INTO EMPLOYEE values(1,NULL,NULL,'user1.assignment@test.com', 'user1', 'a
 INSERT INTO EMPLOYEE values(2,NULL,NULL,'user2.assignment@test.com', 'user2', 'assignment', 123.0,'test user 2', 1, NULL);
 INSERT INTO EMPLOYEE values(3,NULL,NULL,'user3.assignment@test.com', 'user3', 'assignment', 123.0,'test user 3', 1, NULL);
 
-INSERT INTO SECURITY_USER VALUES (1, 'PBKDF2WithHmacSHA256:2048:XH1IRCybcpMMdIyPb6S4gupqzW0cFy942EP0hnRklCc=:APlLoh304bD1zRKOYAVgnD9mWvXvCPW9RjzqHtMpeYI=', 'user1', 1);
+
+INSERT INTO SECURITY_USER VALUES (1, 'PBKDF2WithHmacSHA256:2048:p7zZfR2SgUvR/Fi1K1knXjRDLz8oRRmeIpZCy/DGWwE=:JADPpsNyfpq2BSNvCD/1bwMgVaCnB7eTcguO0ViVJTo=', 'user1', 1);
 INSERT INTO SECURITY_USER VALUES (2, 'PBKDF2WithHmacSHA256:2048:xHT5hKIZlvl29wfD7IkEecWPsPMtWEVbuL3J/Kv3enw=:GtwAap6eOV8aXrgr/YolsFNX4FHJG+byn/Ijpe5m7P0=', 'admin', NULL);
 
-INSERT INTO SECURITY_USER_SECURITY_ROLE values(2,2);
+INSERT INTO SECURITY_USER_SECURITY_ROLE values(1,1);
 INSERT INTO SECURITY_USER_SECURITY_ROLE values(2,2);
